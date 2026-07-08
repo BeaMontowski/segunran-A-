ba5955691bcab8151703d5176eccd0a76bcb99f2
Beatriz <beatriz.montowski@escola.pr.gov.br>
2026-07-08 02:05:55 +0000

    AULA 7

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
+const forcaSenha = document.querySelector('.forca');
+
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
@@ -63,6 +65,20 @@ function geraSenha() {
     }
 
     campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha() {
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+    if (tamanhoSenha > 11) {
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5) {
+        forcaSenha.classList.add('fraca');
+    }
 }
 
 geraSenha();
+
